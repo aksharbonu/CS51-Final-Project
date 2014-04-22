f424340644a33b33d5eacd90d5706f07d7402e9c
Jayant Sani <jayant.sani@gmail.com>
2014-04-22 11:34:38 -0400

    Complete LU Decomposition Algorithm

--- LU.ml
+++ LU.ml
@@ -1,21 +1,18 @@
 let LU_decomp (m : MATRIX) =
-	let (upper, lower) = create_triangular m in
-	(upper, lower)
-	(* TODO - the actual LU decomposition algorithm *)
-
-let create_triangular (m : Matrix) in
-	let row = Array.length m in
-	let col = Array.length m.(0) in 
-	let upper = Array.copy m in
-	    for i = 0 to row - 1 do
-	   		for j = 0 to col - 1 do
-	   			if i >= j then upper.(i).(j) <- 0
-	   		done;
-	   	done;
+	let rows = Array.length m in
+	let cols = Array.length m.(0) in
 	let lower = Array.copy m in
-		for i = 0 to row - 1 do
-	   		for j = 0 to col - 1 do
-	   			if i <= j then lower.(i).(j) <- 0
-	   		done;
-	   	done;
-	(lower, upper)
+	let upper = Array.copy m in
+	for k = 0 to rows - 1 do
+		lower.(k).(k) = 1
+		for i = k + 1 to cols - 1 do
+			lower.(i).(k) = (Float.of_int m.(i).(k))/m.(k).(k) (* How are we handling different types here with division? *)
+			for j = k + 1 to cols - 1 do
+				m.(i).(j) = m.(i).(j) - lower.(i).(k) * m.(k).(j) (* How are we passing in the type of the element so that we know what operations to perform? *)
+			done;
+		done;
+		for j = k to cols - 1 do
+			upper.(k).(j) = m.(k).(j)
+		done;
+	done;
+	(upper, lower)
