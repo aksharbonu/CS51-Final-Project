7f51183330583e3f415ee39988a3fce3c2390873
aksharbonu <akshar.bonu@gmail.com>
2014-04-23 14:05:49 -0400

    updated akshar testing

--- Akshar-Testing.ml
+++ Akshar-Testing.ml
@@ -74,20 +74,22 @@ exception IncompatibleDimensions
             done;
             result;;
 
+
+        let dim m1 = Array.length m1, Array.length m1.(0);; 
+
         let scalar value m1 =
-            let row = Array.length m1 in
-            let col = Array.length m1.(0) in 
+            let row, col = dim m1 in
             let result = zero row col in
-                for i = 0 to Array.length m1 - 1 do
-                    for j = 0 to Array.length m1.(0) - 1 do
+                for i = 0 to row - 1 do
+                    for j = 0 to col - 1 do
                         result.(i).(j) <- M.mul m1.(i).(j) value
                     done;
                 done;
             result;;
 
+
 let do_operation m1 m2 operation = 
-    let row = Array.length m1 in
-    let col = Array.length m1.(0) in 
+    let row, col = dim m1 in
     if row = Array.length m2 && col = Array.length m2.(0) then
         (let result = zero row col in
         for i = 0 to row - 1 do
@@ -106,16 +108,16 @@ let sub m1 m2 =
 
 (* Adds the child matrix to the parent matrix starting at index (row, col) *)
 let join parent child row col =  
-    for i = 0 to Array.length child - 1 do
-        for j = 0 to Array.length child.(0) - 1 do
+    let row, col = dim child in
+    for i = 0 to row - 1 do
+        for j = 0 to col - 1 do
             parent.(i + row).(j + col) <- child.(i).(j) 
         done;
      done; parent;;
 
 (* Checks if dimensions are both 1 (base case) or odd dimensions and makes even *)
 let pad m1 = 
-    let row1 = Array.length m1 in
-    let col1 = Array.length m1.(0) in
+    let row1, col1 = dim m1 in
     if row1 = col1 && row1 = 1 then m1
     else 
     (match row1 mod 2 = 0, col1 mod 2 = 0 with
@@ -126,18 +128,17 @@ let pad m1 =
 
 (* Adds the parent matrix to the child matrix starting at index (row, col) till child is full *)
 let split parent child row col =
-    for i = 0 to Array.length child - 1 do
-        for j = 0 to Array.length child.(0) - 1 do
+    let row, col = dim child in 
+    for i = 0 to row - 1 do
+        for j = 0 to col - 1 do
             child.(i).(j) <- parent.(i + row).(j + col)
         done;
      done; child;;
 
 let rec mul_invariant matrix1 matrix2 =
     (* Saves rows & columns of matrices for future use*)
-    let row1 = Array.length matrix1 in
-    let col1 = Array.length matrix1.(0) in
-    let row2 = Array.length matrix2 in
-    let col2 = Array.length matrix2.(0) in
+    let row1, col1 = dim matrix1 in
+    let row2, col2 = dim matrix2 in
     let result = zero row1 row1 in
     if row1 = 1 then 
         (result.(0).(0) <- M.mul matrix1.(0).(0) matrix2.(0).(0); result)
@@ -215,7 +216,8 @@ let rec mul_invariant matrix1 matrix2 =
         let m1_padded = pad m1 in
         let m2_padded = pad m2 in
         let result_padded = mul_invariant m1_padded m2_padded in
-        let result = zero (Array.length m1) (Array.length m2.(0)) in
+        if dim m1 = dim m1_padded && dim m2 = dim m2_padded then result padded
+        else let result = zero (Array.length m1) (Array.length m2.(0)) in
         let _ = split result_padded result 0 0 in
         result;;
 
