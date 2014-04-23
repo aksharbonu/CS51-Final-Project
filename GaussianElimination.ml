9e3294261eae87cf1cc1609a22ff744b45845620
Jayant Sani <jayant.sani@gmail.com>
2014-04-22 23:20:05 -0400

    Finish Gaussian

--- GaussianElimination.ml
+++ GaussianElimination.ml
@@ -1,9 +1,9 @@
 (* TODO *)
 let solve (m : Matrix) (b : array) =
-    let length = b.length in
+    let length = Array.length b in
     for i = 0 to length - 1 do
         let max = i in
-        for j = i + 1 tolength - 1 do
+        for j = i + 1 to length - 1 do
             if (abs m.(j).(i)) > (abs m.(max).(i)) then max = i
         done;
 
@@ -15,20 +15,23 @@ let solve (m : Matrix) (b : array) =
             b.(max) <- temp_b;
 
         for j = i + 1 to length - 1 do
-            let alpha = (Float.of_int m.(j).(i)) / m.(i).(i) in
-            b.(j) <- b.(j) - alpha * b.(i)
+            let factor = M.div m.(j).(i)) m.(i).(i) in
+            b.(j) <- M.sub b.(j) (M.mul factor b.(i));
             for p = i to length - 1 do
-                m.(j).(p) <- m.(j).(p) - alpha * m.(i).(p)
+                m.(j).(p) <- M.sub m.(j).(p) (M.mul factor m.(i).(p));
             done;
         done;    
     done; 
 
+    let soluton = Array.make length 0 in
     for i = rows - 1 downto 0 do
         let sum = 0.0 in
         for j = i + 1 to rows - 1 do
-            sum = sum + a.(i).(j) *  
+            sum = M.add sum (M.mul a.(i).(j) solution.(j))
         done;
+        solution.(i) <- M.div (M.sub b.(i) sum) m.(i).(i);
     done;
+    (m, b)
 
 (* JAVA CODE TEMPLATE
 // Gaussian elimination with partial pivoting
