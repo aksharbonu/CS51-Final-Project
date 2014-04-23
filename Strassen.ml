866b6d1dd715300dae94114f5e232b62838e023d
aksharbonu <akshar.bonu@gmail.com>
2014-04-22 22:03:03 -0400

    Strassen with dynamic padding

--- Strassen.ml
+++ Strassen.ml
@@ -1,3 +1,34 @@
+        exception IncompatibleDimensions
+
+        type elt = M.t
+
+        type t = elt array array 
+
+        let of_array = ident;;
+
+        let to_array = ident;;
+
+        let zero n m = Array.make_matrix n m M.zero;;
+
+        let identity n =
+            let result = zero n n in
+            for i = 0 to n - 1 do
+                result.(i).(i) <- M.one
+            done;
+            result;;
+
+        let scalar value m1 =
+        let row = Array.length m1 in
+        let col = Array.length m1.(0) in 
+        let result = zero row col in
+            for i = 0 to row - 1 do
+                for j = 0 to col - 1 do
+                    result.(i).(j) <- M.mul m1.(i).(j) value
+                done;
+            done;
+            result
+        end
+
 let do_operation m1 m2 operation = 
     let row = Array.length m1 in
     let col = Array.length m1.(0) in 
@@ -17,23 +48,41 @@ let add m1 m2 =
 let sub m1 m2 = 
         do_operation m1 m2 M.sub;;
 
-    (* TODO *)
-    (* Check if they can multiply *)
-    (* If they can, pad them if necessary *)
-    (* Remove padding using split *)
+let fill result child = 
+    for i = 0 to Array.length child - 1 do
+        for j = 0 to Array.length m1.(0) - 1 do
+            result.(i).(j) <- child.(i).(j)
+        done;
+    done; result;; 
+
+let pad m1 = 
+    if row1 = col1 && row1 = 1 then m1
+    else 
+    (let row1 = Array.length m1 in
+    let col1 = Array.length m1.(0) in
+    match row1 mod 2 = 0, col1 mod 2 = 0 with
+    | true, true -> m1
+    | true, false -> fill (zero (row1 + 1) col1) m1
+    | false, true -> fill (zero row1 (col1 + 1)) m1
+    | _, _ ->  fill (zero (row1 + 1) (col1 + 1)) m1)
+
+let remove_pad result result_padded =
+    for i = 0 to Array.length result - 1 do
+        for j = 0 to Array.length result.(0) - 1 do
+            result.(i).(j) <- result_padded.(i).(j)
+        done;
+    done;
 
 let split parent child row col = 
-    let last = Array.length child - 1 in
-    for i = 0 to last do
-        for j = 0 to last do
+    for i = 0 to Array.length child - 1 do
+        for j = 0 to Array.length child.(0) - 1 do
             child.(i).(j) <- parent.(i + row).(j + col)
         done;
     done;;
 
 let join parent child row col = 
-    let last = Array.length child - 1 in
-    for i = 0 to last do
-        for j = 0 to last do
+    for i = 0 to Array.length child - 1 do
+        for j = 0 to Array.length child.(0) - 1 do
             parent.(i + row).(j + col) <- child.(i).(j) 
         done;
     done;;
@@ -80,13 +129,13 @@ let rec mul_invariant matrix1 matrix2 =
               M7 = (a12 - a22) (b21 + b22)
         *)
 
-        let m1 = mul_invariant (add a11 a22) (add b11 b22) in 
-        let m2 = mul_invariant (add a21 a22) b11 in 
-        let m3 = mul_invariant a11 (sub b12 b22) in
-        let m4 = mul_invariant a22 (sub b21 b11) in
-        let m5 = mul_invariant (add a11 a12) b22 in
-        let m6 = mul_invariant (sub a21 a11) (add b11 b12) in
-        let m7 = mul_invariant (sub a12 a22) (add b21 b22) in
+        let m1 = mul_pad (add a11 a22) (add b11 b22) in 
+        let m2 = mul_pad (add a21 a22) b11 in 
+        let m3 = mul_pad a11 (sub b12 b22) in
+        let m4 = mul_pad a22 (sub b21 b11) in
+        let m5 = mul_pad (add a11 a12) b22 in
+        let m6 = mul_pad (sub a21 a11) (add b11 b12) in
+        let m7 = mul_pad (sub a12 a22) (add b21 b22) in
 
         (*
           C11 = M1 + M4 - M5 + M7
@@ -105,7 +154,21 @@ let rec mul_invariant matrix1 matrix2 =
         join result c21 dim 0; 
         join result c22 dim dim;
 
-        result and
+        result 
 
+    and
+
+    (* Remove & add padding *)
+    mul_pad m1 m2 =
+        let m1_padded = pad m1 in
+        let m2_padded = pad m2 in
+        let result_padded = mul_invariant m1_padded m2_padded in
+        let result = zero (Array.length m1) (Array.length m2.(0))
+        remove_pad result result_padded; result;;
+
+    (* Check if multiplication can be done *)
     mul m1 m2 =
-    mul_invariant m1 m2;; 
+        let row = Array.length m1 in
+        let col = Array.length m1.(0) in
+        if col = Array.length m2 then mul_pad m1 m2
+        else raise IncompatibleDimensions
